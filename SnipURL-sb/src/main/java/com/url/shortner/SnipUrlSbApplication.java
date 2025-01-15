3631eb2243ac7e8998163dcdb43b9e6eff381fb9
Dibyendu-Merlock <107022870+Dibyendu-Merlock@users.noreply.github.com>
2025-01-15 19:38:29 +0530

    Add files via upload

---
+++ SnipURL-sb/src/main/java/com/url/shortner/SnipUrlSbApplication.java
@@ -0,0 +1,13 @@
+package com.url.shortner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnipUrlSbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnipUrlSbApplication.class, args);
+    }
+
+}
